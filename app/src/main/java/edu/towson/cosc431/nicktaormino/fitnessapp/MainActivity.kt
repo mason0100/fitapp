472e7aeed9a8c38a443f9e6f11b1fa68ff4a278b
Nick_Taormino2 <ntaorm1@students.towson.edu>
2019-10-12 18:19:02 -0400

    initial commit

---
+++ app/src/main/java/edu/towson/cosc431/nicktaormino/fitnessapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.towson.cosc431.nicktaormino.fitnessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
